b1487acc00aad7e1d00e93549ea3029ed673b1b1
TheMaxON <sldpsnow8800@gmail.com>
2023-03-02 20:43:47 +0200

    final

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(currentTime, 1000));
+
+function currentTime(data) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data.seconds));
+  console.log(data.seconds);
+}
+
+let time = localStorage.getItem(STORAGE_KEY);
+
+player.setCurrentTime(JSON.parse(time));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const formData = {
+  email: '',
+  message: '',
+};
+const STORAGE_KEY = 'feedback-form-state';
+const parsedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+populateForm();
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateForm() {
+  if (parsedData) {
+    const formKeys = Object.keys(parsedData);
+    formKeys.map(element => {
+      document.querySelector(`[name='${element}']`).value = parsedData[element];
+    });
+  }
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  console.log(formData);
+
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
